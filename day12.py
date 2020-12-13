3d172b586bfdc8a8aee209d8d1845879a96de5d2
Tobias <tobias.heintz@gmail.com>
2020-12-13 23:59:48 +0100

    Day 12 part 2

--- day12.py
+++ day12.py
@@ -6,6 +6,7 @@ directions = {
     "R": ["N", "E", "S", "W", "N", "E", "S", "W"]
 }
 
+# part 1
 class Ship:
     x: int = 0
     y: int = 0
@@ -44,13 +45,64 @@ class Ship:
             exit("unknown cmd " + cmd)
 
 
-# part 1
 ship = Ship()
 
 for v in vals:
-    cmd = v[0]
-    steps = int(v[1:])
-    
-    ship.apply_cmd(cmd, steps)
+    ship.apply_cmd(v[0], int(v[1:]))
 
-print(ship.x + ship.y)
+print(abs(ship.x + ship.y))
+
+# part 2
+class Ship2(Ship):
+    way_x = 10
+    way_y = -1
+
+    def move_to_way(self, steps:int) -> None:
+        self.x += self.way_x * steps
+        self.y += self.way_y * steps
+
+    def move_way(self, dir:str, steps:int) -> None:
+        if dir == "N":
+            self.way_y -= steps
+        elif dir == "E":
+            self.way_x += steps
+        elif dir == "S":
+            self.way_y += steps
+        elif dir == "W":
+            self.way_x -= steps
+        else:
+            exit("unknown dir " + dir)
+    
+    def rot_way(self, dir:str, steps:int) -> None:
+        # probably a nicer way here, but it's late
+        old_x = self.way_x
+        old_y = self.way_y
+
+        if (dir == "R" and steps == 1) or (dir == "L" and steps == 3):
+            self.way_x = -old_y
+            self.way_y = old_x
+        elif (dir == "R" and steps == 2) or (dir == "L" and steps == 2):
+            self.way_x = -old_x
+            self.way_y = -old_y
+        elif (dir == "R" and steps == 3) or (dir == "L" and steps == 1):
+            self.way_x = old_y
+            self.way_y = -old_x
+        else:
+            exit("unknown combination")
+
+    def apply_cmd2(self, cmd:str, steps:int) -> None:
+        if cmd == "F":
+            self.move_to_way(steps)
+        elif cmd in ["N", "E", "S", "W"]:
+            self.move_way(cmd, steps)
+        elif cmd in ["L", "R"]:
+            self.rot_way(cmd, int(steps / 90))
+        else:
+            exit("unknown cmd " + cmd)
+
+ship = Ship2()
+
+for v in vals:
+    ship.apply_cmd2(v[0], int(v[1:]))
+
+print(abs(ship.x + ship.y))
