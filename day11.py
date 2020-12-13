318e85d49b6e47cc6a00618b7789054eea2bddae
Tobias <tobias.heintz@gmail.com>
2020-12-13 21:03:05 +0100

    Day 11 working solution

--- day11.py
+++ day11.py
@@ -1,4 +1,4 @@
-with open("day11-input2.txt") as f:
+with open("day11-input.txt") as f:
     seats = [[char for char in l] for l in f.read().splitlines()]
 
 max_x = len(seats[0]) - 1
@@ -60,53 +60,37 @@ def print_seats(seats:list) -> None:
 
 # part 1
 # print_seats(seats)
-# prev_iter = seats
-# while True:
-#     cur_iter = iter_seats(prev_iter.copy())
-#     # print_seats(cur_iter)
+prev_iter = seats
+while True:
+    cur_iter = iter_seats(prev_iter.copy())
+    # print_seats(cur_iter)
 
-#     if cmp_seats(cur_iter, prev_iter):
-#         cnt = sum([sum([1 if s == "#" else 0 for s in l]) for l in cur_iter])
-#         print(cnt)
-#         break
+    if cmp_seats(cur_iter, prev_iter):
+        cnt = sum([sum([1 if s == "#" else 0 for s in l]) for l in cur_iter])
+        print(cnt)
+        break
 
-#     prev_iter = cur_iter
+    prev_iter = cur_iter
 
 # part 2
 def adj(x:int, y:int, dir:str) -> list:
     # returns a list of tuples with all possible adjacency indices for a given direction
     if dir == "W":
-        while x > 0:
-            x -= 1
-            yield (x, y)
+        return [(nx, y) for nx in range(x - 1, -1, -1)]
     elif dir == "N":
-        while y > 0:
-            y -= 1
-            yield (x, y)
+        return [(x, ny) for ny in range(y - 1, -1, -1)]
     elif dir == "E":
-        while x < max_x:
-            x += 1
-            yield (x, y)
+        return [(nx, y) for nx in range(x + 1, max_x + 1, 1)]
     elif dir == "S":
-        while y < max_y:
-            y += 1
-            yield (x, y)
+        return [(x, ny) for ny in range(y + 1, max_y + 1, 1)]
     elif dir == "NW":
-        while x > 0:
-            x -= 1
-            yield from adj(x, y, "N")
+        return zip(range(x - 1, -1, -1), range(y - 1, -1, -1))
     elif dir == "NE":
-        while x < max_x:
-            x += 1
-            yield from adj(x, y, "N")
+        return zip(range(x + 1, max_x + 1, 1), range(y - 1, -1, -1))
     elif dir == "SE":
-        while x < max_x:
-            x += 1
-            yield from adj(x, y, "S")
+        return zip(range(x + 1, max_x + 1, 1), range(y + 1, max_y + 1, 1))
     elif dir == "SW":
-        while x > 0:
-            x -= 1
-            yield from adj(x, y, "S")
+        return zip(range(x - 1, -1, -1), range(y + 1, max_y + 1, 1))
     else:
         exit("wrong dir")
 
@@ -116,18 +100,17 @@ def adj_seats2(x:int, y:int, seats:list) -> list:
     for dir in dirs:
         for nx, ny in adj(x, y, dir):
             if seats[ny][nx] != ".":
-                print(dir, nx, ny)
                 yield seats[ny][nx]
                 break
 
 def num_adj_occ2(x:int, y:int, seats:list) -> int:
     return sum([1 if s == "#" else 0 for s in adj_seats2(x, y, seats)])
 
-print_seats(seats)
+# print_seats(seats)
 prev_iter = seats
 while True:
     cur_iter = iter_seats(prev_iter.copy(), seat_counter=num_adj_occ2, thres_vis=5)
-    print_seats(cur_iter)
+    # print_seats(cur_iter)
 
     if cmp_seats(cur_iter, prev_iter):
         cnt = sum([sum([1 if s == "#" else 0 for s in l]) for l in cur_iter])
